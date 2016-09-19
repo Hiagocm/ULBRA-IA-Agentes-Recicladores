e38e86a40698949348a3d3aa324df05156b6a13c
Ezequiel <ezequielmross@gmail.com>
2016-09-19 19:56:40 -0300

    render mode up

--- renderizacao.js
+++ renderizacao.js
@@ -1,9 +1,12 @@
-var c = document.getElementById("myCanvas"),
-    ctx = c.getContext("2d");
-/*
-(function renderLoop() {
+var c = document.getElementById("visual"),
+    canvas = c.getContext("2d"),
+    count = 0;
+
+setInterval(function () {
     'use strict';
-    window.requestAnimationFrame(renderLoop);
-    update();
-    draw();
-}());*/
+    canvas.clearRect(0, 0, 500, 400);
+    canvas.fillStyle = "red";
+    canvas.fillRect(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), 10, 10);
+    document.getElementById("ciclo").innerHTML = count;
+    count++;
+}, 1000);
