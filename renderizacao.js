6540d4c7499986608ceffe24da5602b8f4430251
Ezequiel <ezequielmross@gmail.com>
2016-09-19 19:30:01 -0300

    update

---
+++ renderizacao.js
@@ -0,0 +1,9 @@
+var c = document.getElementById("myCanvas"),
+    ctx = c.getContext("2d");
+/*
+(function renderLoop() {
+    'use strict';
+    window.requestAnimationFrame(renderLoop);
+    update();
+    draw();
+}());*/
